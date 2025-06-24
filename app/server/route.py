9e2fae7b5d7c388b8d84386fd55d8ef2464cfab8
Re3ue <re3ue2024@gmail.com>
2025-06-24 09:06:32 +0900

    HTML + CSS

--- app/server/route.py
+++ app/server/route.py
@@ -7,6 +7,8 @@ from flask import Blueprint, render_template
 
 blueprint_route = Blueprint('blueprint_route', __name__)
 
+# All
+
 @blueprint_route.route('/')
 def main() :
     return render_template('index.html')
@@ -22,3 +24,23 @@ def profile() :
 @blueprint_route.route('/sign_in')
 def sign_in() :
     return render_template('sign_in.html')
+
+@blueprint_route.route('/sign_up')
+def sign_up() :
+    return render_template('sign_up.html')
+
+# All - Post + ID
+
+@blueprint_route.route('/post/<int:post_id>')
+def post(post_id) :
+    return render_template('post.html')
+
+# Authenticate
+
+@blueprint_route.route('/create_post')
+def create_post() :
+    return render_template('create_post.html')
+
+@blueprint_route.route('/edit_post')
+def edit_post() :
+    return render_template('edit_post.html')
